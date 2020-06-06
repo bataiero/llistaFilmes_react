6a8f0fa4a3b544ae0390e21f6753c1e02d7b2e36
Rafael Oliveira <rafael.oliveira@nb-roliveira.grupoiv2.local>
2020-06-06 17:21:53 -0300

    v1

---
+++ src/pages/Erro/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Erro extends Component {
+    render() {
+        return (
+            <div>
+                <strong>Página Não Encontrada!</strong>
+            </div>
+        );
+    }
+}
+
+export default Erro;
